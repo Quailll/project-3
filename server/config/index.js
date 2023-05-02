f33b22d640aba6138e91dee4042b72b76d040da5
kyle-mucerino <kyle.mucerino@gmail.com>
2023-05-01 17:38:20 -0700

    config/index.js MongoDB connection

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || process.env.LOCAL_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://kylemucerino:7576Kobe@moviereviews.8a1ifev.mongodb.net/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
