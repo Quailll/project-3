7703d6073a0c356926c081ca394e3bde28599929
thandyn <nguyenphuthan@gmail.com>
2023-05-03 18:53:47 -0700

    search function is working

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import TrendingMovie from "../../components/TrendingMovies";
+import MovieList from "../../components/MovieList";
+import Search from "../../components/Search";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
   return (
     <>
       <div>
         <h2>Homepage</h2>
       </div>
+      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
       <TrendingMovie />
+      <MovieList searchValue={searchValue} />
     </>
   );
 }
